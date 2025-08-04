4e47e2c6b317184540d22955615ceeb45b7f1e67
sireeshamunnamgi-07 <sireeshamunnamgi05@gmail.com>
2025-08-04 15:50:50 +0530

    Added Week-2 folder

---
+++ Week-2/Week2_PLSQL/Exercise 1 - Control Structures/code/scenario01.sql
@@ -0,0 +1,14 @@
+-- Apply 1% Interest Discount for Senior Citizens 
+
+
+BEGIN
+    FOR customer_rec IN (SELECT CustomerID, InterestRate, Age FROM Customers) LOOP
+        IF customer_rec.Age > 60 THEN
+            UPDATE Customers
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = customer_rec.CustomerID;
+        END IF;
+    END LOOP;
+    COMMIT;
+END;
+/
---
+++ Week-2/Week2_PLSQL/Exercise 3 - Stored Procedures/code/insertSampleData.sql
@@ -0,0 +1,24 @@
+-- Savings accounts
+INSERT INTO Accounts VALUES (101, 'Alice Biswas', 'Savings', 10000);
+INSERT INTO Accounts VALUES (102, 'Raj Verma', 'Savings', 15000);
+INSERT INTO Accounts VALUES (103, 'Neha Sharma', 'Savings', 20000);
+INSERT INTO Accounts VALUES (104, 'Sudipa Sarkar', 'Savings', 25000);
+
+-- Current accounts (should not be affected by interest)
+INSERT INTO Accounts VALUES (105, 'Vikram Pal', 'Current', 50000);
+INSERT INTO Accounts VALUES (106, 'Sunita Roy', 'Current', 60000);
+INSERT INTO Accounts VALUES (107, 'Sunidhi Chauhan', 'Current', 80000);
+
+---------------------------------------------
+
+-- Department 10
+INSERT INTO Employees VALUES (201, 'Anil Kumble', 10, 50000);
+INSERT INTO Employees VALUES (202, 'Shankar Das', 10, 60000);
+INSERT INTO Employees VALUES (203, 'Sudip Sarkar', 10, 65000);
+INSERT INTO Employees VALUES (204, 'Sudip Sarkar', 10, 65000);
+
+
+-- Department 20
+INSERT INTO Employees VALUES (205, 'Manoj Sinha', 20, 55000);
+INSERT INTO Employees VALUES (206, 'Sabita Sarkar', 20, 62000);
+INSERT INTO Employees VALUES (207, 'Sabita Sarkar', 20, 62000);
